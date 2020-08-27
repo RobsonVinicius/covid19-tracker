03be38d2b30efd95851a54e2b5f268cbdf983fbe
Robson Vinicius <robsondgn@outlook.com>
2020-08-27 14:21:13 -0300

    Instalando axios

--- src/App.js
+++ src/App.js
@@ -6,14 +6,25 @@ import React from 'react';
 
 /* Importando tudo de um arquivo só */
 import { Cards, Chart, CountryPicker } from './components';
-
 import styles from './App.module.css';
+import { fetchData } from './api';
 
 class App extends React.Component {
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchData = await fetchData();
+
+    this.setState({ data: fetchData });
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <div className={styles.container} /*previnir interferência*/>
-        <Cards />
+        <Cards data={data}/>
         <CountryPicker />
         <Chart />
       </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const cards = () => {
+const cards = (props) => {
+  console.log(props);
   return (
+    
     <h1>Cards</h1>
   )
 }
