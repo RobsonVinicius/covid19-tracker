9290fcd71f81b6a5051b948f1ab81a30cd006cd9
Robson Vinicius <robsondgn@outlook.com>
2020-08-29 21:14:50 -0300

    Criando gráfico de linha, Infectados e Mortos

--- src/api/index.js
+++ src/api/index.js
@@ -12,3 +12,21 @@ export const fetchData = async () => {
 
   }
 }
+
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    const modefiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modefiedData;    
+
+  } catch (error) {
+
+  }
+}
